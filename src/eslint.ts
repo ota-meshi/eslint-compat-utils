bdd21fdba44576ef4c2948afef33d230e2c8d531
ota-meshi <otameshiyo23@gmail.com>
2023-10-08 21:52:43 +0900

    feat: improve getESLint

--- src/eslint.ts
+++ src/eslint.ts
@@ -59,11 +59,31 @@ function getESLintClassForV6() {
       }
     }
 
-    public lintText(
+    // eslint-disable-next-line @typescript-eslint/require-await -- ignore
+    public async lintText(
       ...params: Parameters<eslint.ESLint["lintText"]>
     ): ReturnType<eslint.ESLint["lintText"]> {
       const result = this.engine.executeOnText(params[0], params[1]?.filePath);
       return result.results;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/require-await -- ignore
+    public async lintFiles(
+      ...params: Parameters<eslint.ESLint["lintFiles"]>
+    ): ReturnType<eslint.ESLint["lintFiles"]> {
+      const result = this.engine.executeOnFiles(
+        Array.isArray(params[0]) ? params[0] : [params[0]],
+      );
+      return result.results;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/require-await -- ignore
+    public static async outputFixes(
+      ...params: Parameters<(typeof eslint.ESLint)["outputFixes"]>
+    ): ReturnType<(typeof eslint.ESLint)["outputFixes"]> {
+      return CLIEngine.outputFixes({
+        results: params[0],
+      } as any);
     }
   }
 
