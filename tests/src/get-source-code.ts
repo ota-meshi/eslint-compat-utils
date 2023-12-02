35131aeecfa3824c53531fc01bbaf5f516c5f2d0
Yosuke Ota <otameshiyo23@gmail.com>
2023-12-02 09:35:48 +0900

    test: add tests (#15)
    * test: add tests
    
    * test: add test

--- tests/src/get-source-code.ts
+++ tests/src/get-source-code.ts
@@ -1,6 +1,7 @@
 import assert from "assert";
 import type * as ESTree from "estree";
 import { getSourceCode } from "../../src";
+import type { Scope } from "eslint";
 import { Linter } from "eslint";
 
 describe("getSourceCode", () => {
@@ -34,6 +35,55 @@ describe("getSourceCode", () => {
           "VariableDeclarator",
         ],
       );
+    });
+  });
+  describe("getDeclaredVariables", () => {
+    it("The result of getDeclaredVariables should match your expectations.", () => {
+      const linter = new Linter();
+      let variables: Scope.Variable[] = [];
+      linter.defineRule("test-rule", {
+        create(context) {
+          return {
+            VariableDeclaration(node: ESTree.VariableDeclaration) {
+              variables = getSourceCode(context).getDeclaredVariables(node);
+            },
+          };
+        },
+      });
+      linter.verify("var {x,y} = {}", {
+        parserOptions: { ecmaVersion: 2015 },
+        rules: {
+          "test-rule": "error",
+        },
+      });
+
+      assert.strictEqual(variables.length, 2);
+      assert.deepStrictEqual(
+        variables.map((n) => n.name),
+        ["x", "y"],
+      );
+    });
+  });
+  describe("markVariableAsUsed", () => {
+    it("The result of markVariableAsUsed should match your expectations.", () => {
+      const linter = new Linter();
+      linter.defineRule("test-rule", {
+        create(context) {
+          return {
+            "Identifier[name=me]"(node: ESTree.Identifier) {
+              getSourceCode(context).markVariableAsUsed(node.name, node);
+            },
+          };
+        },
+      });
+      const result = linter.verify("var me", {
+        rules: {
+          "test-rule": "error",
+          "no-unused-vars": "error",
+        },
+      });
+
+      assert.strictEqual(result.length, 0);
     });
   });
 });
