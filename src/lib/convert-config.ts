4ed49537cb7ab37a764ec13971d09579b40346d2
Yosuke Ota <otameshiyo23@gmail.com>
2024-06-01 12:55:39 +0900

    fix: resolve globals (#44)
    * fix: resolve globals
    
    * Create two-doors-jump.md

--- src/lib/convert-config.ts
+++ src/lib/convert-config.ts
@@ -3,6 +3,106 @@ import * as semver from "semver";
 import type { LinterConfigForV8 } from "../v8-props";
 import { safeRequire, safeRequireResolve } from "./require";
 
+const builtInGlobals = new Map<number, [string, boolean][]>([
+  [
+    3,
+    Object.entries({
+      Array: false,
+      Boolean: false,
+      constructor: false,
+      Date: false,
+      decodeURI: false,
+      decodeURIComponent: false,
+      encodeURI: false,
+      encodeURIComponent: false,
+      Error: false,
+      escape: false,
+      eval: false,
+      EvalError: false,
+      Function: false,
+      hasOwnProperty: false,
+      Infinity: false,
+      isFinite: false,
+      isNaN: false,
+      isPrototypeOf: false,
+      Math: false,
+      NaN: false,
+      Number: false,
+      Object: false,
+      parseFloat: false,
+      parseInt: false,
+      propertyIsEnumerable: false,
+      RangeError: false,
+      ReferenceError: false,
+      RegExp: false,
+      String: false,
+      SyntaxError: false,
+      toLocaleString: false,
+      toString: false,
+      TypeError: false,
+      undefined: false,
+      unescape: false,
+      URIError: false,
+      valueOf: false,
+    }),
+  ],
+  [
+    5,
+    Object.entries({
+      JSON: false,
+    }),
+  ],
+  [
+    2015,
+    Object.entries({
+      ArrayBuffer: false,
+      DataView: false,
+      Float32Array: false,
+      Float64Array: false,
+      Int16Array: false,
+      Int32Array: false,
+      Int8Array: false,
+      Intl: false,
+      Map: false,
+      Promise: false,
+      Proxy: false,
+      Reflect: false,
+      Set: false,
+      Symbol: false,
+      Uint16Array: false,
+      Uint32Array: false,
+      Uint8Array: false,
+      Uint8ClampedArray: false,
+      WeakMap: false,
+      WeakSet: false,
+    }),
+  ],
+  [
+    2017,
+    Object.entries({
+      Atomics: false,
+      SharedArrayBuffer: false,
+    }),
+  ],
+  [
+    2020,
+    Object.entries({
+      BigInt: false,
+      BigInt64Array: false,
+      BigUint64Array: false,
+      globalThis: false,
+    }),
+  ],
+  [
+    2021,
+    Object.entries({
+      AggregateError: false,
+      FinalizationRegistry: false,
+      WeakRef: false,
+    }),
+  ],
+]);
+
 /** Convert to eslintrc config from v9 config  */
 export function convertConfigToRc(
   config: eslint.Linter.FlatConfig | eslint.Linter.FlatConfig[],
@@ -36,6 +136,16 @@ export function convertConfigToRc(
       ...languageOptions,
       ...parserOptions,
       ...(newConfig as LinterConfigForV8).parserOptions,
+    };
+    const resolvedEcmaVersion = (newConfig as LinterConfigForV8).parserOptions!
+      .ecmaVersion! as number;
+    (newConfig as LinterConfigForV8).globals = {
+      ...Object.fromEntries(
+        [...builtInGlobals.entries()].flatMap(([version, editionGlobals]) =>
+          resolvedEcmaVersion < version ? [] : editionGlobals,
+        ),
+      ),
+      ...(newConfig as LinterConfigForV8).globals,
     };
     if (globals) {
       (newConfig as LinterConfigForV8).globals = {
