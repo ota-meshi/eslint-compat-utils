aab37beb94d99ec357fb65b926c10232bf277120
Yosuke Ota <otameshiyo23@gmail.com>
2024-11-28 13:23:08 +0900

    fix: wrong shim for esm (#60)
    * fix: wrong shim for esm
    
    * Create orange-mice-ring.md

---
+++ .changeset/orange-mice-ring.md
@@ -0,0 +1,5 @@
+---
+"eslint-compat-utils": patch
+---
+
+fix: wrong shim for esm
--- src/lib/get-unsupported.ts
+++ src/lib/get-unsupported.ts
@@ -1,9 +1,6 @@
+import { safeRequire } from "./require";
+
 /** Get "eslint/use-at-your-own-risk" */
 export function getUnsupported(): any {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports -- ignore
-    return require("eslint/use-at-your-own-risk");
-  } catch {
-    return {};
-  }
+  return safeRequire("eslint/use-at-your-own-risk") || {};
 }
