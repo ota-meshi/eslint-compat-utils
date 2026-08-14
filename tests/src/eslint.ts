a90e506617ba2343d8efdc398b3a43a47d0c20fa
yosuke ota <otameshiyo23@gmail.com>
2026-08-14 10:50:53 +0900

    chore: fix lint errors

--- tests/src/eslint.ts
+++ tests/src/eslint.ts
@@ -23,7 +23,7 @@ describe("getESLint", () => {
                 },
               },
             },
-          } as any,
+          },
           rules: {
             "test/test": "error",
             "no-undef": "error",
@@ -69,7 +69,7 @@ describe("getESLint", () => {
                   },
                 },
               },
-            } as any,
+            },
             rules: {
               "test/test": "error",
               "no-undef": "error",
